d0231de083e2cb868ba1c10acc69f629be99ca67
Colson <colsoncalifornication@gmail.com>
2023-08-12 08:23:47 +0545

    create class sorter to implement sort

--- build/index.js
+++ build/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log('sort');
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() { }
+}
+const sorter = new Sorter([9, 2, 5, 0, -3, 6, 4, 8, 1, 3]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('sort');
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {}
+}
+
+const sorter = new Sorter([9, 2, 5, 0, -3, 6, 4, 8, 1, 3]);
+sorter.sort();
+console.log(sorter.collection);
