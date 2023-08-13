bd4dec888442091693da83be097734fc469977ab
Colson <colsoncalifornication@gmail.com>
2023-08-13 09:41:32 +0545

    implement characters collection to sort strings

--- build/index.js
+++ build/index.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
-const NumbersCollection_1 = require("./NumbersCollection");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([
-    9, 2, 5, 0, -3, 6, 4, 8, 1, 3,
-]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
+const CharactersCollection_1 = require("./CharactersCollection");
+// const numbersCollection = new NumbersCollection([
+//   9, 2, 5, 0, -3, 6, 4, 8, 1, 3,
+// ]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data );
+const charactersCollection = new CharactersCollection_1.CharacterCollection('MacBook');
+const sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharacterCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([
-  9, 2, 5, 0, -3, 6, 4, 8, 1, 3,
-]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([
+//   9, 2, 5, 0, -3, 6, 4, 8, 1, 3,
+// ]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data );
+
+const charactersCollection = new CharacterCollection('MacBook');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data );
+console.log(charactersCollection.data)
